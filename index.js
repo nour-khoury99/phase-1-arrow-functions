43107e2ae24271e1ae3962ec23e2a84af04abb52
nour khoury <khourynour79@gmail.com>
2022-09-14 17:22:04 +0300

    done

---
+++ index.js
@@ -0,0 +1,25 @@
+function foo(){
+    return 'bar';
+}
+
+const foro = function() {
+    return 'bar';
+  }
+
+
+  const sum = (parameter1,parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 +parameter2;
+  }
+
+
+  console.log(3,4);
+
+  const nums = [1,2,3];
+  const squares = nums.map(x => x**2);
+  squares;
+  nums;
+
+
+  
